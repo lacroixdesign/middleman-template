743e0432210894604f07688539792050ff6e6247
Michael LaCroix <michael@lacroixdesign.net>
2013-11-05 18:58:07 -0600

    switch to slim

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "https://rubygems.org"
-ruby "1.9.3"
 
-gem "middleman", "~>3.0.11"
-# gem "middleman-livereload"
-gem "middleman-deploy"
-gem "boarding_pass"
-gem "highline"
+gem 'middleman'
+# gem 'middleman-livereload'
+gem 'middleman-deploy'
+gem 'boarding_pass'
+gem 'highline'
+gem 'slim'
--- lib/application_helper.rb
+++ lib/application_helper.rb
@@ -1,52 +1,14 @@
 module ApplicationHelper
 
-  # Page Titles - Set individual page title elements
-  # Accepts a String or Array.
-  # Sets yield(:title) to a String for use in <title>.
-  # 
-  #   --Array--
-  #   title ["Example", "Nashville, TN"]
-  #   => "Example - Page - Title"
-  # 
-  #   --String--
-  #   title "Example Page Title"
-  #   => "Example Page Title"
-  # 
-  def title title_partials
-    title = if title_partials.is_a? String
-      title_partials
-    elsif title_partials.is_a? Array
-      title_partials.reject(&:blank?).join(' - ')
-    end
-    content_for(:title) { title }
+  # Browser title
+  def title(site_title)
+    page_title = current_page.data.page_title || current_page.data.title
+    [page_title, site_title].reject(&:blank?).join(' | ')
   end
-
 
   # Display IcoMoon font icon
-  # 
   def icon key
     "<i data-icon=&#x#{h(key)}></i>"
-  end
-  
-
-  # Date: November 5, 2013
-  # 
-  def date_full(date)
-    date.strftime("%B %e, %Y") unless date.blank?
-  end
-
-
-  # Date: Nov 5, 2013
-  # 
-  def date_short(date)
-    date.strftime("%b %e, %Y") unless date.blank?
-  end
-
-
-  # Date: 11/5/2013
-  # 
-  def date_compact(date)
-    date.strftime("%-m/%-d/%Y") unless date.blank?
   end
 
 end
